fc89261a4355f822dd177dee5999f1b8ea0a846d
bartek <bartosz.krawczyk@hatimeria.pl>
2014-08-13 11:40:00 +0200

    before new PraphicProduct.php

--- GraphicProduct.php
+++ GraphicProduct.php
@@ -14,7 +14,13 @@ class GraphicProduct implements Product
 
     public function getProperties()
     {
-        $this->mfgProduct="This is a graphic.<3";
+        $this->mfgProduct="<!doctype html><html><head><meta charset='UTF-8' />";
+        $this->mfgProduct.="<title>Map Factory</title>";
+        $this->mfgProduct.="</head><body>";
+        $this->mfgProduct.="<img src='ml-map.gif' width='500' height='500' alt='mali map'/>";
+        $this->mfgProduct.="<h1>This is a graphic.<3</h1>";
+        $this->mfgProduct.="</body></html>";
+
         return $this->mfgProduct;
     }
 }
